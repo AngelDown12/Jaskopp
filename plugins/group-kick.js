537ccde53ccfff4dd04251cd2a74620826632335
AngelDown12 <angeldown12xz@gmail.com>
2025-07-21 23:48:17 -0600

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -4,7 +4,7 @@ const handler = async (m, { conn }) =>
        m.reply('☁️ Intruso eliminado.'))
     : m.reply('☁️ _Menciona al usuario que deseas eliminar._');
 
-handler.command = ['kick'] 
+handler.command = ['kick', 'prepusio'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
